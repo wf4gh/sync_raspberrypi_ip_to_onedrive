3304ec89f7f6a53f7907dfc28c35bf813adf5bed
wf <mytemp2@163.com>
2019-05-16 14:41:34 +0800

    authenticate every time before upload

--- sync.py
+++ sync.py
@@ -5,15 +5,18 @@ import settings
 from time import sleep
 from datetime import datetime
 
-# authentication
-redirect_uri = 'http://localhost:8080/'
-client_secret = settings.CLIENT_SECRET
-scopes = ['wl.signin', 'wl.offline_access', 'onedrive.readwrite']
-client = onedrivesdk.get_default_client(
-    client_id=settings.CLIENT_ID, scopes=scopes)
-auth_url = client.auth_provider.get_auth_url(redirect_uri)
-code = GetAuthCodeServer.get_auth_code(auth_url, redirect_uri)
-client.auth_provider.authenticate(code, redirect_uri, client_secret)
+
+def auth():
+    # authentication
+    redirect_uri = 'http://localhost:8080/'
+    client_secret = settings.CLIENT_SECRET
+    scopes = ['wl.signin', 'wl.offline_access', 'onedrive.readwrite']
+    client = onedrivesdk.get_default_client(
+        client_id=settings.CLIENT_ID, scopes=scopes)
+    auth_url = client.auth_provider.get_auth_url(redirect_uri)
+    code = GetAuthCodeServer.get_auth_code(auth_url, redirect_uri)
+    client.auth_provider.authenticate(code, redirect_uri, client_secret)
+    return client
 
 
 def get_ip():
@@ -29,12 +32,13 @@ def upload_file():
         drive='me', id='root').children['Raspberry_Pi_IP.txt'].upload('./ip.txt')
 
 
-sync_interval = settings.SYNC_INTERVAL * 60
+sync_interval = max(5*60, settings.SYNC_INTERVAL*60) # limit sync every 5 minutes
 sync_days = settings.SYNC_DAYS
 
 start_time = datetime.now()
 
 while not sync_days or (datetime.now() - start_time).days <= sync_days:
+    client = auth()
     get_ip()
     upload_file()
     sleep(sync_interval)
